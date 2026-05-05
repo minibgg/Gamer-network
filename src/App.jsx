3eb27aeead777b6a796c520a263f1583581f7c9c
minibgg <ivangey984@gmail.com>
2026-05-05 05:22:56 +0300

    base ui add password input

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,61 @@
-import { useState, useEffect, use } from "react";
+import { useState } from "react";
 import "./App.css";
-import "./components/Fetches.js";
 import { fetchGet, fetchPost } from "./components/Fetches.js";
 
 export default function UserInput() {
   const [inputEmail, setInputEmail] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
   const [inputUserName, setInputUserName] = useState("");
   const [inputRiotGames, setInputRiotGames] = useState("");
-  const [data, setData] = useState("");
+  const [users, setUsers] = useState([]);
 
   console.log(inputEmail, inputUserName, inputRiotGames);
 
   async function FetchPostUserData() {
-    const createdUser = await fetchPost.postUserData({
-      name: inputUserName,
+    await fetchPost.postUserData({
       email: inputEmail,
+      password: inputPassword,
+      name: inputUserName,
       riotUser: inputRiotGames,
     });
-    const users = fetchGet.getUsers();
-    console.log(users);
-    return FetchPostUserData;
+    const freshUsers = await fetchGet.getUsers();
+    setUsers(freshUsers);
+
+    console.log(freshUsers);
   }
 
   return (
     <main>
-      <div>
-        <input
-          placeholder="RiotGames#RU1"
-          style={{ border: "2px solid pink" }}
-          value={inputRiotGames}
-          onChange={(e) => setInputRiotGames(e.target.value)}
-        />
-        <br />
-        <input
-          placeholder="Email"
-          style={{ border: "2px solid violet" }}
-          value={inputEmail}
-          onChange={(e) => setInputEmail(e.target.value)}
-        />
-        <br />
-        <input
-          placeholder="userName"
-          style={{ border: "2px solid red" }}
-          value={inputUserName}
-          onChange={(e) => setInputUserName(e.target.value)}
-        />
-        <br />
-        <button onClick={FetchPostUserData}>Register</button>
+      <div className="mainBorder">
+        <div>Gamer network</div>
+        <div>
+          <input
+            placeholder="Email"
+            value={inputEmail}
+            onChange={(e) => setInputEmail(e.target.value)}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            value={inputPassword}
+            onChange={(e) => setInputPassword(e.target.value)}
+          />
+          <br />
+          <input
+            placeholder="userName"
+            value={inputUserName}
+            onChange={(e) => setInputUserName(e.target.value)}
+          />
+          <br />
+          <input
+            placeholder="RiotGames#RU1"
+            value={inputRiotGames}
+            onChange={(e) => setInputRiotGames(e.target.value)}
+          />
+          <br />
+          <button onClick={FetchPostUserData}>Register</button>
+        </div>
       </div>
     </main>
   );
