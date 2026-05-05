62ac1897eb409e246e9d91e21d40c3ef9ff236f0
minibgg <ivangey984@gmail.com>
2026-05-05 14:05:31 +0300

    user link

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from "react";
 import { fetchGet, fetchPost } from "../components/Fetches.js";
+import "./Users.css";
 
 export default function Users() {
   const [data, setData] = useState([]);
   const [users, setUsers] = useState([]);
 
   async function LoadUsers() {
-    const [fetchUsers] = await Promise.all([fetchGet.getUsers()]);
+    const [fetchedUsers] = await Promise.all([fetchGet.getUsers()]);
     setData({
-      fetchUsers,
+      fetchedUsers,
     });
+    console.log(data.fetchUsers);
   }
 
   return (
-    <button style={{ margin: "20px" }} onClick={LoadUsers}>
+    <button className="loadData" style={{ margin: "20px" }} onClick={LoadUsers}>
       Load Users
     </button>
   );
