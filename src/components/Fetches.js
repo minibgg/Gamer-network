32e175ecf0827e0ff401b20e2e2a5209a6c1c5bb
minibgg <ivangey984@gmail.com>
2026-05-11 14:12:12 +0300

    commit

---
+++ src/components/Fetches.js
@@ -0,0 +1,25 @@
+export const fetchPost = {
+  async postUserData(userData) {
+    const response = await fetch(
+      "https://gamernetwork-production.up.railway.app/api/users",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      },
+    );
+
+    const data = await response.json();
+    return data;
+  },
+};
+
+export const fetchGet = {
+  async getUser() {
+    const res = await fetch(
+      `https://gamernetwork-production.up.railway.app/api/users`,
+    );
+  },
+};
