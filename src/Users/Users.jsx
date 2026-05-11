3b9fb951566342020dfd160f8853ea577aa1e19a
minibgg <ivangey984@gmail.com>
2026-05-11 09:15:53 +0300

    link users output

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -1,22 +1,39 @@
-import { useEffect, useState } from "react";
-import { fetchGet, fetchPost } from "../components/Fetches.js";
+import { useState } from "react";
+import { fetchGet } from "../components/Fetches.js";
 import "./Users.css";
 
 export default function Users() {
-  const [data, setData] = useState([]);
   const [users, setUsers] = useState([]);
 
   async function LoadUsers() {
-    const [fetchedUsers] = await Promise.all([fetchGet.getUsers()]);
-    setData({
-      fetchedUsers,
-    });
-    console.log(data.fetchUsers);
+    try {
+      const fetchedUsers = await fetchGet.getUsers();
+      setUsers(fetchedUsers);
+    } catch (error) {
+      console.log("backend error");
+    }
   }
 
   return (
-    <button className="loadData" style={{ margin: "20px" }} onClick={LoadUsers}>
-      Load Users
-    </button>
+    <main>
+      <button
+        className="loadData"
+        style={{ margin: "20px" }}
+        onClick={LoadUsers}
+      >
+        Load Users
+      </button>
+      <div className="usersBorder">
+        {users.map((user) => (
+          <div key={user.id} className="userBorder">
+            <div>{user.email}</div>
+            <div>{user.role}</div>
+            <div>{user.status}</div>
+            <div>{user.emailVerified}</div>
+            <div>{user.createdAt}</div>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
